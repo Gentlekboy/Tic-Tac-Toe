98ec336773ab04e30a715ecf02afbaa2cdcc70b1
gentlekboy <gentlekboy@gmail.com>
2021-08-29 23:45:23 +0100

    added rate us, report bug and exit buttons

--- app/src/main/java/com/gentlekboy/tictactoe/ui/LandingPageActivity.kt
+++ app/src/main/java/com/gentlekboy/tictactoe/ui/LandingPageActivity.kt
@@ -1,40 +1,57 @@
 package com.gentlekboy.tictactoe.ui
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.gentlekboy.tictactoe.R
+import android.view.View
+import com.gentlekboy.tictactoe.databinding.ActivityLandingPageBinding
 
 /*
 This activity serves as the game's main menu
 It contains navigation links to play the game, rate the app, share the app, report a bug and exit the app
  */
+
+private lateinit var binding: ActivityLandingPageBinding
+
 class LandingPageActivity : AppCompatActivity() {
-    /**
-     * Create an instance of play button
-     */
-    private lateinit var instanceOfPlayButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landing_page)
+        binding = ActivityLandingPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        /**
-         * Grab Id of play button
-         */
-
-        instanceOfPlayButton = findViewById(R.id.playButtonId)
-
-        /**
-         * Set onClickListener on the button
-         */
-
-        instanceOfPlayButton.setOnClickListener { toSelectPlayerActivity() }
+        binding.playButton.setOnClickListener { navigateToSelectPlayerActivity() }
+        binding.rateButton.setOnClickListener { rateUsOnPlayStore() }
+        binding.reportBugButton.setOnClickListener { reportBug() }
+        binding.exitButton.setOnClickListener { exitGame() }
     }
 
-    private fun toSelectPlayerActivity() {
-        var intent = Intent(this, SelectPlayerActivity::class.java)
+    private fun navigateToSelectPlayerActivity() {
+        startActivity(Intent(this, SelectPlayerActivity::class.java))
+    }
+
+    private fun rateUsOnPlayStore() {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(
+                "https://play.google.com/store/apps/details?id=com.example.android")
+            setPackage("com.android.vending")
+        }
         startActivity(intent)
+    }
+
+    private fun reportBug() {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_EMAIL, arrayOf("kufreabasi.udoh@decagon.dev", "seun.awonugba@decagon.dev"))
+            putExtra(Intent.EXTRA_SUBJECT, "BUG REPORT - TIC TAC TOE")
+            type = "message/rfc822"
+        }
+
+        startActivity(Intent.createChooser(intent, "Send report with"))
+    }
+
+    private fun exitGame() {
+        finish()
     }
 }
