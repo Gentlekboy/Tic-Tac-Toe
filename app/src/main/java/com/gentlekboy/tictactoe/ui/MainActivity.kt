9a7472f61f8a845c6d2477478ae264e134209820
gentlekboy <gentlekboy@gmail.com>
2021-08-29 23:46:01 +0100

    designed splash screen

--- app/src/main/java/com/gentlekboy/tictactoe/ui/MainActivity.kt
+++ app/src/main/java/com/gentlekboy/tictactoe/ui/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.gentlekboy.tictactoe.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.gentlekboy.tictactoe.R
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.gentlekboy.tictactoe.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
 //This activity holds a splash screen that welcomes users to the app
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        //Navigate to main menu page
         Handler().postDelayed({
-            val intent = Intent(this, LandingPageActivity::class.java)
-            startActivity(intent)
-        }, 1500)
+            startActivity(Intent(this, LandingPageActivity::class.java))
+            finish()
+        }, 2_000)
     }
 }
