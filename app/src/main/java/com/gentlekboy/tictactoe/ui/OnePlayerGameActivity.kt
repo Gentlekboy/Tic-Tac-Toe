efcf3b6d43304ef0e2894627e089392584a15e64
gentlekboy <gentlekboy@gmail.com>
2021-08-30 02:10:26 +0100

    added a click button function

--- app/src/main/java/com/gentlekboy/tictactoe/ui/OnePlayerGameActivity.kt
+++ app/src/main/java/com/gentlekboy/tictactoe/ui/OnePlayerGameActivity.kt
@@ -2,11 +2,40 @@ package com.gentlekboy.tictactoe.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import com.gentlekboy.tictactoe.R
+import com.gentlekboy.tictactoe.databinding.ActivityOnePlayerGameBinding
+
+private lateinit var binding: ActivityOnePlayerGameBinding
 
 class OnePlayerGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_one_player_game)
+        binding = ActivityOnePlayerGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+    fun clickButton(view: View){
+        val selectedButton = view as Button
+        var cellId = 0
+
+        when(selectedButton){
+            binding.buttonOne -> cellId = 1
+            binding.buttonTwo -> cellId = 2
+            binding.buttonThree -> cellId = 3
+            binding.buttonFour -> cellId = 4
+            binding.buttonFive -> cellId = 5
+            binding.buttonSix -> cellId = 6
+            binding.buttonSeven -> cellId = 7
+            binding.buttonEight -> cellId = 8
+            binding.buttonNine -> cellId = 9
+        }
+
+        Log.d("GKB", "clickButton: $selectedButton")
+        Log.d("GKB", "clickButton: $cellId")
     }
 }
